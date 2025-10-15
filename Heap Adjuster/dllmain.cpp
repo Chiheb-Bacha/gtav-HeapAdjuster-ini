24d4896b133047f49167b2910f2478ade55c454d
Chiheb Bacha <chiheb@ElBachino.localdomain>
2025-10-15 15:35:20 +0200

    Added support for Enhanced

--- Heap Adjuster/dllmain.cpp
+++ Heap Adjuster/dllmain.cpp
@@ -1,19 +1,20 @@
 #include <dllmain.h>
 #include <iostream>
 #include <fstream>
+#include "Globals.h"
 
 // working around a bug in Hooking.Patterns x64 version
 template<typename T = void>
-auto get_pattern(const char *pattern_string, ptrdiff_t offset = 0)
+auto get_pattern(const char* pattern_string, ptrdiff_t offset = 0)
 {
 	return hook::pattern(pattern_string).get_first<T>(offset);
 }
 
-int heapSize = GetPrivateProfileInt("HEAP_SETTINGS", "HEAP_SIZE", 650, ".\\HeapAdjuster.ini");
+int heapSize = GetPrivateProfileInt("HEAP_SETTINGS", "HEAP_SIZE", 1024, ".\\HeapAdjuster.ini");
 int vanillaHeapSize;
 
 using namespace std;
-void Logging() 
+void Logging()
 {
 	ofstream myfile;
 	myfile.open("HeapAdjuster.log");
@@ -24,13 +25,24 @@ void Logging()
 
 void InitializeMod()
 {
-	int* loc = hook::get_pattern<int32_t>("83 C8 01 48 8D 0D ? ? ? ? 41 B1 01 45 33 C0", 17);
+	int* loc;
+	if (g_isEnhanced)
+	{
+		loc = hook::get_pattern<int32_t>("ba ? ? ? ? 45 31 c0 41 b1 01 e8 ? ? ? ? 48 8d 0d", 1);
+	}
+	else
+	{
+		loc = hook::get_pattern<int32_t>("83 C8 01 48 8D 0D ? ? ? ? 41 B1 01 45 33 C0", 17);
+	}
+
 	vanillaHeapSize = *loc / 1024 / 1024;
 
 	DWORD oldProtect;
 	VirtualProtect(loc, 4, PAGE_EXECUTE_READWRITE, &oldProtect);
 
-	*loc = heapSize * 1024 * 1024; // default in 323 was 412 MiB
+	*loc = heapSize * 1024 * 1024;
+	// default in 889.22 Enhanced was 648MiB, 
+	// default in v3586.0 Legacy was 478MiB
 
 	VirtualProtect(loc, 4, oldProtect, &oldProtect);
 
